a49e9eaa4ad3e40642cc36f55d32e3e61ef52091
Olena Iliushyn <lenailyshun@gmail.com>
2026-03-09 20:24:32 +0200

    Intro to Entity framework

---
+++ 05_EntityFramework/Airplane.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _05_EntityFramework
+{
+    //Entity - Airplane
+    class Airplane
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Model { get; set; }
+        //Relationship type : one to many (1...*)
+        public ICollection<Flight> Flights { get; set; }
+    }
+}
---
+++ 05_EntityFramework/Client.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _05_EntityFramework
+{
+    [Table("Passangers")]
+    class Client
+    {
+        //Primary Key naming : Id/id/ID/ EntityName+Id (ClientId)
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Column("FirstName")]
+        public string Name { get; set; }
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+        public DateTime? Birthdate { get; set; }//value type null
+        //null ---> not null
+        //not null ---> null
+
+        //Relationship type : many to many (*...*)
+        public ICollection<Flight> Flights { get; set; }
+    }
+}
