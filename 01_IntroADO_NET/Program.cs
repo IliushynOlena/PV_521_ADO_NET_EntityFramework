422c5e00f140991a866a76578503c450099526e1
Olena Iliushyn <lenailyshun@gmail.com>
2026-03-20 18:56:13 +0200

    Use Dapper technology

--- 01_IntroADO_NET/Program.cs
+++ 01_IntroADO_NET/Program.cs
@@ -1,8 +1,24 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 using System.Text;
+using Dapper;
 
 namespace _01_IntroADO_NET
 {
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+        public int CostPrice { get; set; }
+        public string Producer { get; set; }
+        public int Price { get; set; }
+        public override string ToString()
+        {
+            return $"Name:{Name,10} | Type:{Type,10} | Q:{Quantity,5}| Price: {CostPrice,5}| Producer :{Producer,10}";
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -12,11 +28,11 @@ namespace _01_IntroADO_NET
             //Initial Catalog - name database
             //Integrated Sequrity - windows user - true / owervise - false
             //login - password
-            string connectionString = @"Data Source=DESKTOP-1LCG8OH\SQLEXPRESS;Initial Catalog=SportShop;Integrated Security=True;Connect Timeout=5;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            /*string connectionString = @"Data Source=DESKTOP-1LCG8OH\SQLEXPRESS;Initial Catalog=SportShop;Integrated Security=True;Connect Timeout=5;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             SqlConnection con = new SqlConnection(connectionString);
             con.Open();
             Console.WriteLine("Connected success!!!!");
-
+            */
             //insert , update, delete
             #region Execute Non-Query
             //string cmdText = @"insert into Products
@@ -34,13 +50,13 @@ namespace _01_IntroADO_NET
             //Console.WriteLine($"Average price : {res}");
             #endregion
             #region Execute Reader
-
+            /*
             string cmdCommand = @"SELECT * FROM Products;";
             SqlCommand command = new SqlCommand(cmdCommand, con);
 
             SqlDataReader reader =  command.ExecuteReader();   
             Console.OutputEncoding = Encoding.UTF8;
-           
+
             for (int i = 0; i < reader.FieldCount; i++)
             {
                 //Console.Write(reader.GetName(i) + "\t");
@@ -56,14 +72,72 @@ namespace _01_IntroADO_NET
                 Console.WriteLine();
 
             }
-      
+
             reader.Close();
 
 
 
-            #endregion
+
             //working with database
             con.Close();    
+            */
+            #endregion
+
+            string connectionString = @"Data Source=DESKTOP-1LCG8OH\SQLEXPRESS;Initial Catalog=SportShop;Integrated Security=True;Connect Timeout=5;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var users = db.Query<Product>("SELECT Id, Name FROM Products");
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+            //string cmdText = $@"insert into Products
+            //                values('{product.Name}',
+            //                       '{product.Type}',
+            //                        {product.Quantity},
+            //                        {product.CostPrice},
+            //                       '{product.Producer}',
+            //                       {product.Price})";
+            //string cmdText = @"insert into Products
+            //values('Snowboard New','winter Equipment',15,15000,'Italy',12000)";
+
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                string sql = @"INSERT INTO Products (Name, TypeProduct, Quantity,CostPrice,Producer,Price)
+              VALUES (@Name, @Type, @Quantity,@CostPrice,@Producer,@Price)";
+
+
+                db.Execute(sql, new Product { Name = "New", Type = "New", Quantity  = 1, CostPrice = 1,
+                  Producer = "New", Price = 1});
+            }
+            Console.WriteLine("---------------------------------");
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var users = db.Query<Product>("SELECT Id, Name FROM Products");
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Id + " "+ user.Name);
+                }
+
+                string sql = "UPDATE Products SET Name = @Name WHERE Id = @Id";
+
+                db.Execute(sql, new { Id = 1, Name = "Mike" });
+
+                users = db.Query<Product>("SELECT Id, Name FROM Products");
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Id + " " + user.Name);
+                }
+            }
+
+
+
+
         }
     }
 }
---
+++ 08_DapperTechnology/Program.cs
@@ -0,0 +1,72 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace _08_DapperTechnology
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+        public int CostPrice { get; set; }
+        public string Producer { get; set; }
+        public int Price { get; set; }
+        public override string ToString()
+        {
+            return $"Name:{Name,10} | Type:{Type,10} | Q:{Quantity,5}| Price: {CostPrice,5}| Producer :{Producer,10}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=DESKTOP-1LCG8OH\SQLEXPRESS;Initial Catalog=SportShop;Integrated Security=True;Connect Timeout=5;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                db.Open();
+
+                var products = db.Query<Product>("SELECT Id, Name, Price FROM Products");
+                foreach (Product product in products)
+                {
+                    Console.WriteLine($"Id : {product.Id} . Name : {product.Name}." +
+                        $" Price : {product.Price}");
+                }
+
+                //Insert
+                string sqlCommand = "INSERT INTO Products (Name, TypeProduct, Quantity,CostPrice,Producer,Price)" +
+                    "VALUES (@Name, @Type, @Quantity,@CostPrice,@Producer,@Price)";
+
+                db.Execute(sqlCommand, new Product
+                {
+                    Name = "NewProduct",
+                    Type = "NewType",
+                    Quantity = 1,
+                    CostPrice = 1,
+                    Producer = "New Producer",
+                    Price = 2
+                });
+
+                products = db.Query<Product>("SELECT Id, Name, Price FROM Products");
+                foreach (Product product in products)
+                {
+                    Console.WriteLine($"Id : {product.Id} . Name : {product.Name}." +
+                        $" Price : {product.Price}");
+                }
+
+                sqlCommand = "UPDATE Products SET Price = 200 WHERE Id = @id";
+                db.Execute(sqlCommand,new {Id = 77});
+
+                products = db.Query<Product>("SELECT Id, Name, Price FROM Products");
+                foreach (Product product in products)
+                {
+                    Console.WriteLine($"Id : {product.Id} . Name : {product.Name}." +
+                        $" Price : {product.Price}");
+                }
+
+            }
+        }
+    }
+}
